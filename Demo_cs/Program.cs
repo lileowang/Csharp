78d30c00ff856639782cb94e9f907f9a1c85b51b
lileowang <wangli20@hotmail.com>
2019-05-09 23:20:06 -0700

    Created C# console solution in Visual Studio 2017

---
+++ Demo_cs/Program.cs
@@ -0,0 +1,27 @@
+﻿// Author:      Li Leo Wang
+// Start Date:  2019-05-09
+// Description:
+//      - Demo project for C#  features
+// Notes:
+//      - The individual features are implemented in separated classes.
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+using System;
+using static System.Console;
+
+namespace Demo_cs
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            bool bRet = true;
+
+            WriteLine($"{bRet}");
+            ReadKey();
+            return 0;
+        }
+    }
+}
