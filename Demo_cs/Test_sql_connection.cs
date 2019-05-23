594e8c08ae351935446f3adb6a4bc2e3ba5c8c52
lileowang <wangli20@hotmail.com>
2019-05-23 16:12:55 -0700

    Added class to test SQL connection

---
+++ Demo_cs/Test_sql_connection.cs
@@ -0,0 +1,28 @@
+﻿// Author:      Li Leo Wang
+// Start Date:  2019-05-23
+// Description:
+//      - Demo SqlConnection
+// Notes:
+//      - Prepare database table: testfsn01 and script file
+//      - Prepare script file: c:\temp\test01.sql
+//          select fsn, timeadded
+//          from testfsn01
+//          
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+using static System.Console;
+
+namespace Demo_cs
+{
+    class Test_sql_connection
+    {
+        bool Run()
+        {
+            bool bRet = true;
+
+            return bRet;
+        }
+    }
+}
