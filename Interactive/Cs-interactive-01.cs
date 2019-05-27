9ed61194df97eefd76fef213e22cbfc1fdc73a6e
lileowang <wangli20@hotmail.com>
2019-05-27 15:49:21 -0700

    Added C# interactive (REPL) file with nullable script.

---
+++ Interactive/Cs-interactive-01.cs
@@ -0,0 +1,19 @@
+// Author:      Li Leo Wang
+// Start Date:  2019-05-27
+// Description:
+//      - C# interactive (REPL) demo
+// Notes:
+//      - Visual Studio 2017: Open C# REPL 
+//              by View --> Other windows --> C# interactive
+//      - Copy code without ">" and run it in C# REPL
+//          
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+> int? n1 = null;
+> n1
+null
+> int n2 = n1 ?? 10;
+> n2
+10
