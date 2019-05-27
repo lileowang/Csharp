ba887e095007d19975db982d3637f23e1c8c4e2e
lileowang <wangli20@hotmail.com>
2019-05-27 15:49:21 -0700

    shortcut for console writeline

---
+++ Interactive/Cs-interactive-01.cs
@@ -0,0 +1,26 @@
+// Author:      Li Leo Wang
+// Start Date:  2019-05-27
+// Description:
+//      - C# interactive (REPL) demo
+// Notes:
+//      - Visual Studio 2017: Open C# REPL 
+//              by View --> Other windows --> C# interactive
+//      - Copy code without ">" and run it in C# REPL
+//          
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+// nullable and coalesce
+> int? n1 = null;
+> n1
+null
+> int n2 = n1 ?? 10;
+> n2
+10
+
+// shortcut for console writeline
+> Action<object> cw = Console.WriteLine;
+> cw($"{nameof(n2)} = {n2}")
+n2 = 10
+
