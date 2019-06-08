578f51c47a3105b4e6c9299acc1f28df34ad03fa
lileowang <demo@example.com>
2019-06-07 19:13:24 -0700

    improved comment.

--- Demo_cs/Test_xml_writer.cs
+++ Demo_cs/Test_xml_writer.cs
@@ -1,7 +1,7 @@
 ﻿// Author:      Li Leo Wang
 // Start Date:  2019-06-07
 // Description:
-//      - Demo creating XML for XSL demo
+//      - Demo creating XML for XSL transformation.
 // Notes:
 //      - Prepare database table: testfsn01 and script file
 //      - Prepare script file: c:\temp\test01.sql
