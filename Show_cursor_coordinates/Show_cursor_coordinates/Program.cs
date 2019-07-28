0c9303a4fd9cadc753af9a935db9c886e3fd59d9
lileowang <demo@example.com>
2019-07-28 00:04:01 -0700

    Added show cursor cordinates console program.

---
+++ Show_cursor_coordinates/Show_cursor_coordinates/Program.cs
@@ -0,0 +1,35 @@
+﻿// Author:      Li Leo Wang
+// Start Date:  2019-07-27
+// Description:
+//      - Display cursor coordinates
+// Notes:
+//      - (none)
+//
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace Show_cursor_coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Press Escape to stop");
+
+            while (!(KeyAvailable && ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                WriteLine("x = " + Cursor.Position.X + ", y = " + Cursor.Position.Y);
+                Thread.Sleep(1000);
+            }
+
+            WriteLine("Press any key to quit");
+            ReadKey();
+        }
+    }
+}
